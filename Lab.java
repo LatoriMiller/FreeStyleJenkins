72e25243b9f2d218e014c7ac7633ac63405890d6
LatoriMiller <latori.miller@gmail.com>
2019-01-14 20:51:33 -0500

    first commit

---
+++ Lab.java
@@ -0,0 +1,9 @@
+
+public class Lab {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Don't forget to navigate to the src file when pushing to git!");
+	}
+
+}
